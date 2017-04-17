0d2a7ae30eb43a44c24b1183fd17f4d43dcd6d0a
Dave Love <bungeemonkee@gmail.com>
2017-04-17 17:56:36 -0400

    Set test binary directory manually

--- test.bat
+++ test.bat
@@ -4,11 +4,13 @@ set scripts=%scripts:~0,-1%
 
 set project=%1%.Tests
 
+if not defined framework set framework=netcoreapp1.1
 if not defined configuration set configuration=Release
 
 set assembly=%project%\%project%.csproj
+set output=%project%\bin\%configuration%\%framework%
 
-%scripts%\OpenCover\OpenCover.Console.exe -returntargetcode -register:path32 -target:"dotnet.exe" -targetargs:"test --no-build --logger trx -c %configuration% %assembly% " -output:coverage.xml -oldstyle
+%scripts%\OpenCover\OpenCover.Console.exe -returntargetcode -register:path32 -target:"dotnet.exe" -targetargs:"test --no-build --logger trx -c %configuration% -o %output% %assembly% " -output:coverage.xml -oldstyle
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 %scripts%\coveralls.io\coveralls.net.exe --opencover coverage.xml
